9c03f5cb68a9f63d3bf35138092bfe05579ec2f0
bernardosevero <bernardoseverosilveira@gmail.com>
2024-04-30 23:52:59 -0300

    🎨 chore: setup eslint and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,64 @@
-'use client'
+'use client';
 
-import {useEffect, useRef, useState} from 'react'
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
-  const [isListening, setIsListening ] = useState(false)
-  const [transcript, setTranscript ] = useState('')
-  const recognitionRef = useRef<SpeechRecognition>()
-
+  const [isListening, setIsListening] = useState(false);
+  const [transcript, setTranscript] = useState('');
+  const recognitionRef = useRef<SpeechRecognition>();
 
   useEffect(() => {
-    if('webkitSpeechRecognition' in window) {
-      recognitionRef.current = new window.webkitSpeechRecognition()
-      const recognition = recognitionRef.current as SpeechRecognition
-
-      recognition.interimResults = true
-      recognition.lang = 'pt-BR'
-      recognition.continuous = true
-
+    if ('webkitSpeechRecognition' in window) {
+      recognitionRef.current = new window.webkitSpeechRecognition();
+      const recognition = recognitionRef.current as SpeechRecognition;
+      recognition.interimResults = true;
+      recognition.lang = 'pt-BR';
+      recognition.continuous = true;
 
       recognition.onresult = (event: SpeechRecognitionEvent) => {
-        let text = ''
-        Object.values(event.results).forEach((result: SpeechRecognitionResult) => {
+        let text = '';
 
-          text += result[0].transcript
-        })
+        Object.values(event.results).forEach(
+          (result: SpeechRecognitionResult) => {
+            text += result[0].transcript;
+          }
+        );
 
-        setTranscript(text)
-      }
-
+        setTranscript(text);
+      };
 
       recognition.onend = () => {
-        setIsListening(false)
-      }
+        setIsListening(false);
+      };
 
       return () => {
-        recognition.stop()
-      }
+        recognition.stop();
+      };
     }
-  }, [])
+  }, []);
 
   function startListening() {
-    if(recognitionRef.current && !isListening) {
-      recognitionRef.current.start()
-      setIsListening(true)
+    if (recognitionRef.current && !isListening) {
+      recognitionRef.current.start();
+      setIsListening(true);
     }
   }
 
   function stopListening() {
-    if(recognitionRef.current && !isListening) {
-      recognitionRef.current.stop()
-      setIsListening(false)
+    if (recognitionRef.current && !isListening) {
+      recognitionRef.current.stop();
+      setIsListening(false);
     }
-
   }
 
   return (
     <>
-      <button onClick={() => isListening ? stopListening() : startListening()}>{!isListening? 'Start to record' : 'recording'}</button>
-      <p> 
-        {transcript}
-      </p>
+      <button
+        onClick={() => (isListening ? stopListening() : startListening())}
+      >
+        {!isListening ? 'Start to record' : 'recording'}
+      </button>
+      <p>{transcript}</p>
     </>
   );
 }
